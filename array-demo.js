5a2a10917a8827ede7d3dab606400f703bcc06a2
sayful-hassan <sayf.fashion@gmail.com>
2025-01-02 08:43:25 +0530

    tried out unshift, shift and few more

--- array-demo.js
+++ array-demo.js
@@ -1,11 +1,10 @@
 let favorite_color=['black', 'white', 'beige', 'violet']
 
+
+//for(i=0;i<favorite_color.length;i++){
+//    console.log(favorite_color[i])
+  //  }
+
+favorite_color.unshift('black gold')
 console.log(favorite_color)
 
-for(i=0;i<favorite_color.length;i++){
-    console.log(favorite_color[i])
-    
-}
-
-favorite_color.shit()
-console.log(favorite_color)
